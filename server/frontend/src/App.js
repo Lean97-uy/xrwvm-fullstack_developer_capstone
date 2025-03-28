fb83534ddd8c4e3e395d56d45efcea9158acaf91
Lea97-uy <leandrogar97@gmail.com>
2025-03-28 15:25:28 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,21 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
